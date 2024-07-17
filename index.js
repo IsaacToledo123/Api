4ab996f48511c4d83c9dde45c64ae53005648367
IsaacToledo123 <223245@ids.upchiapas.edu.mx>
2024-07-17 01:43:34 -0600

    cambios

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-
 const port = 3005;
 
 app.use(express.json());
-
 app.use(cors());
 
 const server = app.listen(port, () => {
-  console.log(`api corriendo en el puerto ${port}`);
+  console.log(`API corriendo en el puerto ${port}`);
 });
 
 const ServerWS = new Server(server, {
-  cors: {
+  cors: { 
     origin: "*",
   },
 });
@@ -23,10 +21,29 @@ const ServerWS = new Server(server, {
 ServerWS.on("connection", (socket) => {
   console.log("Cliente conectado");
 
-  socket.on("nuevoVehiculo", (vehiculo) => {
-    console.log("Nuevo vehículo recibido:", vehiculo);
-    // Puedes emitir eventos de vuelta al cliente si es necesario
-     ServerWS.emit("nuevoVehiculo", vehiculo);
+  // Escucha eventos específicos para cada tipo de mensaje
+  socket.on("nivelAgua", (data) => {
+    console.log("Nuevo mensaje de nivel de agua:", data);
+    // Puedes realizar acciones específicas con los datos aquí
+    ServerWS.emit("nuevo", { tipo: "nivelAgua", data });
+  });
+
+  socket.on("ph", (data) => {
+    console.log("Nuevo mensaje de pH:", data);
+    // Puedes realizar acciones específicas con los datos aquí
+    ServerWS.emit("nuevo", { tipo: "ph", data });
+  });
+
+  socket.on("flujoAgua", (data) => {
+    console.log("Nuevo mensaje de flujo de agua:", data);
+  
+    ServerWS.emit("nuevo", { tipo: "flujoAgua", data });
+  });
+
+  socket.on("nivelFertilizante", (data) => {
+    console.log("Nuevo mensaje de nivel de fertilizante:", data);
+ 
+    ServerWS.emit("nuevo", { tipo: "nivelFertilizante", data });
   });
 
   socket.on("disconnect", () => {
